57f657a8062293422b35c197d0b47507cda476cc
Jakub Uryga <urygajakub@gmail.com>
2025-04-11 11:17:53 +0200

    environment config

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+
+async function loadFlyonUI() {
+  return import('flyonui/flyonui');
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const initFlyonUI = async () => {
+      await loadFlyonUI();
+    };
+
+    initFlyonUI();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ul className="timeline overflow-x-auto">
+  <li>
+    <div className="timeline-start text-base-content font-medium">1984</div>
+    <div className="timeline-middle">
+      <span className="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span className="badge badge-primary size-3 rounded-full p-0" />
+      </span>
+    </div>
+    <div className="timeline-end timeline-box">Macintosh PC</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start text-base-content font-medium">1998</div>
+    <div className="timeline-middle">
+      <span className="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span className="badge badge-primary size-3 rounded-full p-0" />
+      </span>
+    </div>
+    <div className="timeline-end timeline-box">iMac</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start text-base-content font-medium">2001</div>
+    <div className="timeline-middle">
+      <span className="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span className="badge badge-primary size-3 rounded-full p-0" />
+      </span>
+    </div>
+    <div className="timeline-end timeline-box">iPod</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start text-base-content font-medium">2007</div>
+    <div className="timeline-middle">
+      <span className="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span className="badge badge-primary size-3 rounded-full p-0" />
+      </span>
+    </div>
+    <div className="timeline-end timeline-box">iPhone</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start text-base-content font-medium">2015</div>
+    <div className="timeline-middle">
+      <span className="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span className="badge badge-primary size-3 rounded-full p-0" />
+      </span>
+    </div>
+    <div className="timeline-end timeline-box">Apple Watch</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start text-base-content font-medium">2024</div>
+    <div className="timeline-middle">
+      <span className="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span className="badge badge-primary size-3 rounded-full p-0" />
+      </span>
+    </div>
+    <div className="timeline-end timeline-box">Vision Pro</div>
+  </li>
+</ul>
+  </>
+  
+
+  );
 }
 
-export default App
+export default App;
